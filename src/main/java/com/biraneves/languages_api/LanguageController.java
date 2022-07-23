28c20944296f5f992884b7154a4b62244e53671b
biraneves <ubirajara.neves@usp.br>
2022-07-23 14:08:43 -0300

    Created LanguageController to return all languages.

---
+++ src/main/java/com/biraneves/languages_api/LanguageController.java
@@ -0,0 +1,24 @@
+package com.biraneves.languages_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LanguageController {
+
+    @GetMapping("/languages")
+    public List<Language> getLanguages() {
+
+        List<Language> languages = List.of(
+                new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
+                new Language("Go", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go/go_256x256.png", 2),
+                new Language("C++", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/cpp/cpp_256x256.png", 3)
+        );
+
+        return languages;
+
+    }
+
+}
