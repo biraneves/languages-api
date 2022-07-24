0c3c41c22b05a10f87c097fd9ac55bc5adea26c2
biraneves <ubirajara.neves@usp.br>
2022-07-24 08:15:53 -0300

    Refactored class LanguageController.

--- src/main/java/com/biraneves/languages_api/LanguageController.java
+++ src/main/java/com/biraneves/languages_api/LanguageController.java
@@ -8,14 +8,14 @@ import java.util.List;
 @RestController
 public class LanguageController {
 
+    private final List<Language> languages = List.of(
+            new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
+            new Language("Go", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go/go_256x256.png", 2),
+            new Language("C++", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/cpp/cpp_256x256.png", 3)
+    );
+
     @GetMapping("/languages")
     public List<Language> getLanguages() {
-
-        List<Language> languages = List.of(
-                new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
-                new Language("Go", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go/go_256x256.png", 2),
-                new Language("C++", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/cpp/cpp_256x256.png", 3)
-        );
 
         return languages;
 
