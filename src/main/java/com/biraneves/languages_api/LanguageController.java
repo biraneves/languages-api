4a5e5eb1a0128c80d4b36d6c51e53124ce4c5604
biraneves <ubirajara.neves@usp.br>
2022-07-24 09:52:41 -0300

    Updated LanguageController.

--- src/main/java/com/biraneves/languages_api/LanguageController.java
+++ src/main/java/com/biraneves/languages_api/LanguageController.java
@@ -1,5 +1,6 @@
 package com.biraneves.languages_api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,16 +9,13 @@ import java.util.List;
 @RestController
 public class LanguageController {
 
-    private final List<Language> languages = List.of(
-            new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
-            new Language("Go", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go/go_256x256.png", 2),
-            new Language("C++", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/cpp/cpp_256x256.png", 3)
-    );
+    @Autowired
+    private LanguageRepository repository;
 
     @GetMapping("/languages")
     public List<Language> getLanguages() {
 
-        return languages;
+        return repository.findAll();
 
     }
 
